2f2f11bc435673e39e930b563dd89b5fe2744514
Daniel Rasmuson <dan123911@gmail.com>
2015-08-09 04:44:09 -0400

    final example of material design before bed

--- Client/main.js
+++ Client/main.js
@@ -117,7 +117,7 @@ angular.module('app')
 
 function TabsCtrl ($scope, $log) {
     var tabs = [
-          { title: 'One', content: "Tabs will become paginated if there isn't enough room for them."},
+          { title: 'Google Drive', content: "Tabs will become paginated if there isn't enough room for them."},
           { title: 'Two', content: "You can swipe left and right on a mobile device to change tabs."},
           { title: 'Three', content: "You can bind the selected tab via the selected attribute on the md-tabs element."},
           { title: 'Four', content: "If you set the selected tab binding to -1, it will leave no tab selected."},
@@ -146,4 +146,8 @@ function TabsCtrl ($scope, $log) {
       var index = tabs.indexOf(tab);
       tabs.splice(index, 1);
     };
+
+    $scope.testClick = function(){
+      alert('test')
+    }
 }
