fdbefc6117577800eafd35be8b5e36346c99c573
Daniel Rasmuson <dan123911@gmail.com>
2015-08-08 23:11:36 -0400

    pulling folders from server

--- Client/main.js
+++ Client/main.js
@@ -1,16 +1,43 @@
 angular.module('app', []);
 
 angular.module('app')
-.controller('BodyController', BodyController);
+.constant('SERVER_DOMAIN', 'http://localhost:3000/')
+.controller('BodyController', BodyController)
+.service('Mindmap', Mindmap)
 
-function BodyController(){
+function BodyController(Mindmap){
   var me = this;
   me.hello = 'hi';
   me.attachDriveFiles = function(){
     swal("Good job!", "You clicked the button!", "success");
   }
 
+  Mindmap
+    .driveFolders()
+    .then(function(folders){
+      console.log('folder');
+      console.log(folders);
+    })
+
   setTimeout(function(){
     document.querySelector('#mindmap').focus();
   }, 1000)
+}
+
+function Mindmap($http, $q, SERVER_DOMAIN){
+  function driveFolders(){
+    var deferred = $q.defer();
+
+    $http
+      .get(SERVER_DOMAIN+'map/drive/folder')
+      .then(function(res){
+        deferred.resolve(res.data);
+      })
+
+    return deferred.promise;
+  }
+
+  return {
+    driveFolders: driveFolders 
+  } 
 }
--- Server/routes/map.js
+++ Server/routes/map.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('this is the map page');
+});
+
+router.get('/drive/folder', function(req, res, next) {
+  res.json([{id: 'sljdflasjdf2343'}]);
 });
 
 module.exports = router;
